0250f04802005116ac8f5a705754d94a400ec55b
shritej11 <shritejdhere11@gmail.com>
2025-03-24 19:43:51 +0530

    Added Video Trailer

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,31 +1,20 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constants'
-import { useDispatch } from 'react-redux';
+import React from 'react'
+import useMovieTrailer from '../hooks/useMovieTrailer';
+import { useSelector } from 'react-redux';
 
 const VideoBackground = ({ movieId }) => {
-
-  const dispatch = useDispatch();
+  const trailerVideo = useSelector(store=>store.movies?.trailerVideo);
   
-  const getMovieVideos = async () => {
-    const data = await fetch('https://api.themoviedb.org/3/movie/950396/videos?language=en-US', API_OPTIONS);
-    const json = await data.json();
-    // console.log(json);
+  useMovieTrailer(movieId);
 
-    const filterData = json.results.filter(video => video.type == "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    // console.log(trailer);
-  };
-  useEffect(() => {
-    getMovieVideos();
-  }, [])
   return (
     <div>
       <iframe
        
-        src={"https://www.youtube.com/embed/"+ trailer.key}
+        src={"https://www.youtube.com/embed/"+ trailerVideo?.key}
         title="YouTube video player" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
+        
         >
       </iframe>
     </div>
